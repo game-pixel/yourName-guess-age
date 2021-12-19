0d5ec2dada60506d3f4f60f30eea152c3ccbc39c
game-pixel <divancoder10@gmail.com>
2021-12-19 03:49:46 +0000

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,24 @@
+<script>
+    let name = ''
+    let age = ''
+
+    async function getAge() {
+        fetch('https://api.agify.io/?name=' + name)
+        .then((response) => response.json())
+        .then((data) => {
+  	age = data.age; // Path for age
+	});
+}
+</script>
+
+<h1>Guess Age</h1>
+
+<form on:submit|preventDefault={getAge}>
+    <label for="name">Name:</label>
+    <input id="name" bind:value={name} type="text" />
+    <button type="submit">Check</button>
+</form>
+
+<p>Age: {age}</p>
+
+
